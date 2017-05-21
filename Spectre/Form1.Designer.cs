bedaa49f2dbd2e78526b3b29741d987c2d34a8d1
TheNnoitra <thennoitra@hotmail.com>
2017-05-22 02:57:59 +0400

    ff

---
+++ Spectre/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Spectre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.BTNOpen = new System.Windows.Forms.Button();
+            this.LBL9 = new System.Windows.Forms.Label();
+            this.Chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ChartFFT = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ChartAFT = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Os = new System.Windows.Forms.Button();
+            this.OsY = new System.Windows.Forms.Button();
+            this.OsZ = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartFFT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartAFT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTNOpen
+            // 
+            this.BTNOpen.Location = new System.Drawing.Point(1076, 12);
+            this.BTNOpen.Name = "BTNOpen";
+            this.BTNOpen.Size = new System.Drawing.Size(75, 23);
+            this.BTNOpen.TabIndex = 0;
+            this.BTNOpen.Text = "Открыть";
+            this.BTNOpen.UseVisualStyleBackColor = true;
+            this.BTNOpen.Click += new System.EventHandler(this.BTNOpen_Click);
+            // 
+            // LBL9
+            // 
+            this.LBL9.AutoSize = true;
+            this.LBL9.Location = new System.Drawing.Point(1089, 392);
+            this.LBL9.Name = "LBL9";
+            this.LBL9.Size = new System.Drawing.Size(0, 13);
+            this.LBL9.TabIndex = 9;
+            // 
+            // Chart4
+            // 
+            this.Chart4.AccessibleRole = System.Windows.Forms.AccessibleRole.Chart;
+            chartArea1.Name = "ChartArea1";
+            this.Chart4.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart4.Legends.Add(legend1);
+            this.Chart4.Location = new System.Drawing.Point(12, 12);
+            this.Chart4.Name = "Chart4";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Ось Х";
+            this.Chart4.Series.Add(series1);
+            this.Chart4.Size = new System.Drawing.Size(1058, 138);
+            this.Chart4.TabIndex = 11;
+            this.Chart4.Text = "График столбца 1";
+            // 
+            // ChartFFT
+            // 
+            this.ChartFFT.AccessibleRole = System.Windows.Forms.AccessibleRole.Chart;
+            chartArea2.Name = "ChartArea1";
+            this.ChartFFT.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.ChartFFT.Legends.Add(legend2);
+            this.ChartFFT.Location = new System.Drawing.Point(12, 337);
+            this.ChartFFT.Name = "ChartFFT";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Фазный спектр";
+            this.ChartFFT.Series.Add(series2);
+            this.ChartFFT.Size = new System.Drawing.Size(1058, 148);
+            this.ChartFFT.TabIndex = 14;
+            this.ChartFFT.Text = "График столбца 1";
+            // 
+            // ChartAFT
+            // 
+            this.ChartAFT.AccessibleRole = System.Windows.Forms.AccessibleRole.Chart;
+            chartArea3.Name = "ChartArea1";
+            this.ChartAFT.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.ChartAFT.Legends.Add(legend3);
+            this.ChartAFT.Location = new System.Drawing.Point(12, 180);
+            this.ChartAFT.Name = "ChartAFT";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Амплитудный спектр";
+            this.ChartAFT.Series.Add(series3);
+            this.ChartAFT.Size = new System.Drawing.Size(1058, 151);
+            this.ChartAFT.TabIndex = 15;
+            this.ChartAFT.Text = "График столбца 1";
+            // 
+            // Os
+            // 
+            this.Os.Location = new System.Drawing.Point(1076, 180);
+            this.Os.Name = "Os";
+            this.Os.Size = new System.Drawing.Size(75, 23);
+            this.Os.TabIndex = 16;
+            this.Os.Text = "Ось X";
+            this.Os.UseVisualStyleBackColor = true;
+            this.Os.Click += new System.EventHandler(this.OsX_Click);
+            // 
+            // OsY
+            // 
+            this.OsY.Location = new System.Drawing.Point(1076, 209);
+            this.OsY.Name = "OsY";
+            this.OsY.Size = new System.Drawing.Size(75, 23);
+            this.OsY.TabIndex = 17;
+            this.OsY.Text = "Ось Y";
+            this.OsY.UseVisualStyleBackColor = true;
+            this.OsY.Click += new System.EventHandler(this.OsY_Click);
+            // 
+            // OsZ
+            // 
+            this.OsZ.Location = new System.Drawing.Point(1076, 238);
+            this.OsZ.Name = "OsZ";
+            this.OsZ.Size = new System.Drawing.Size(75, 23);
+            this.OsZ.TabIndex = 18;
+            this.OsZ.Text = "Ось Z";
+            this.OsZ.UseVisualStyleBackColor = true;
+            this.OsZ.Click += new System.EventHandler(this.OsZ_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(1076, 337);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 19;
+            this.Clear.Text = "Сбросить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 497);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.OsZ);
+            this.Controls.Add(this.OsY);
+            this.Controls.Add(this.Os);
+            this.Controls.Add(this.ChartAFT);
+            this.Controls.Add(this.ChartFFT);
+            this.Controls.Add(this.Chart4);
+            this.Controls.Add(this.LBL9);
+            this.Controls.Add(this.BTNOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartFFT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartAFT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNOpen;
+        private System.Windows.Forms.Label LBL9;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartFFT;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartAFT;
+        private System.Windows.Forms.Button Os;
+        private System.Windows.Forms.Button OsY;
+        private System.Windows.Forms.Button OsZ;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ Spectre/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Spectre
+{
+
+    /// <summary>
+    /// Сама программа
+    /// </summary>
+    /// 
+
+    public partial class Form1 : Form
+    {
+        string input;//переменные
+        int kol = 0;
+         
+
+        public string[,] file = new string[500000, 3];//объявили массив. максимум 500 000 строк 
+        public Form1()
+        {
+            InitializeComponent();
+           
+            using (StreamReader sr = new StreamReader(@"C:\Users\Asus\Desktop\1.txt", Encoding.Default))//открываем файл по адресу записанному в " "
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");//выбирается кодировка. твои числа разделены . а не , по-этому пришлось выбрать такую кодировку
+                int i = 0;
+
+                while ((input = sr.ReadLine()) != null)//цикл который считывает построчно весь файл, попутно считая строки
+                {
+
+                    string[] m = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    for (uint k = 0, j = 0; k < m.Length; k++)
+                    {
+
+                        if (m[k].Trim() != " ")
+                        {
+                            file[i, j] = m[k];
+                            j++;
+                        }
+                    }
+                    i++;
+                    kol = i;
+                } //прочитали файл, сосчитали строки и записали инфу в массив file[i,j], где i - значения, j - номер столбца, откуда считали (0 - Х, 1 - Y, 2 - Z)
+
+            }
+            
+             
+
+        }
+        const int N = 250;
+        double[] Inner = new double[N];
+        Complex[] Furie = new Complex[N];
+
+        private void BTNOpen_Click(object sender, EventArgs e)// тут выполняются операци при нажатии на кнопку - Открыть
+        {
+
+           LBL9.Text = Convert.ToString(kol);//выводится кол-во строк в нашу программу
+                      
+            Chart4.ChartAreas[0].AxisX.Maximum = kol;//ренджируется график для Оси X/Y/Z
+                 
+                     
+                    
+
+        }
+
+        private void Chart1_Click(object sender, EventArgs e)//это случайно образовалось, не удаляй!
+        {
+
+
+        }
+
+
+
+
+        /// <summary>
+        /// Быстрое преобразование Фурье
+        /// </summary>
+        public class Complex
+        {
+            public double Re;// Реальная часть
+            public double Im; // Мнимая
+            public double Amplitude; // Амплитуда для АЧХ
+            public double Faza; // Фаза для ФЧХ
+            public double Frecuensy; // Частота гармоники
+            
+        }
+
+
+        private void OsX_Click(object sender, EventArgs e) //действия при нажатии на кнопку - Ось X (для кнопок Ось Y, Ось Z выполняются точно такие же действия)
+        {
+            
+
+            int N = Inner.Length; //объявляем переменные
+
+            double Arg;
+
+            
+            for (int k = 0; k < N; k++)//цикл вычисления БПФ
+            {
+                
+                Furie[k] = new Complex();//объявляем массив
+                for (int n = 0; n < N; n++)//цикл расчета/записи массива
+                {
+                   
+                    Arg = 2 * Math.PI * k * n / N;                   
+                    Furie[k].Re += Convert.ToDouble(file[n, 0]) * Math.Cos(Arg);
+                    Furie[k].Im -= Convert.ToDouble(file[n, 0]) * Math.Sin(Arg);
+                    
+
+                }
+
+                Furie[k].Amplitude = (Math.Sqrt(Math.Pow(Furie[k].Re, 2) + Math.Pow(Furie[k].Im, 2))) ;//нахождение амплитуды
+                Furie[k].Faza = Math.Atan2(Furie[k].Im, Furie[k].Re) / Math.PI * 180;//нахождение фазы
+                Furie[k].Frecuensy = ((N - 1) * (k));// нахождение частоты
+
+            }//тт мы типа произвели БПФ, осталось вывести полученные данные
+
+            Chart4.Series[0].LegendText = "Ось X"; //задаем имя для первого графика
+            for (uint i = 1; i < kol; i++)//отрисовываем первый график, для Оси
+            {
+                Chart4.Series[0].Points.AddXY(i, Convert.ToDouble(file[i, 0]));//выбираем где и как рисовать, говорит ему какие точки соединить линией
+
+            }
+
+            for (int i = 0; i < N; i++)//тут отрисовываем 2й и 3й графики
+            {
+
+                ChartAFT.Series[0].Points.AddXY(Furie[i].Frecuensy, Furie[i].Amplitude);//строим Амплитудный спектр
+                ChartFFT.Series[0].Points.AddXY(Furie[i].Frecuensy, Furie[i].Faza);// строим Фазный спектр
+            }
+
+        }
+
+        private void OsY_Click(object sender, EventArgs e)//выполняются действия при нажатии на кнопку ОсьY
+        {
+
+
+            int N = Inner.Length;
+
+            double Arg;
+
+
+            for (int k = 0; k < N; k++)
+            {
+
+                Furie[k] = new Complex();
+                for (int n = 0; n < N; n++)
+                {
+                    Arg = 2 * Math.PI * k * n / N;
+                    Furie[k].Re += Convert.ToDouble(file[n, 1]) * Math.Cos(Arg);
+                    Furie[k].Im -= Convert.ToDouble(file[n, 1]) * Math.Sin(Arg);
+
+
+                }
+
+                Furie[k].Amplitude = (Math.Sqrt(Math.Pow(Furie[k].Re, 2) + Math.Pow(Furie[k].Im, 2)));
+                Furie[k].Faza = Math.Atan2(Furie[k].Im, Furie[k].Re) / Math.PI * 180;
+                Furie[k].Frecuensy = ((N - 1) * (k));
+
+            }
+
+            Chart4.Series[0].LegendText = "Ось Y";
+            for (uint i = 1; i < kol; i++)
+            {
+                Chart4.Series[0].Points.AddXY(i, Convert.ToDouble(file[i, 1]));
+
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+
+                ChartAFT.Series[0].Points.AddXY(Furie[i].Frecuensy, Furie[i].Amplitude);
+                ChartFFT.Series[0].Points.AddXY(Furie[i].Frecuensy, Furie[i].Faza);
+            }
+        }
+
+        private void OsZ_Click(object sender, EventArgs e)//выполняются действия при нажатии на кнопку ОсьZ
+        {
+
+
+            int N = Inner.Length;
+
+            double Arg;
+
+
+            for (int k = 0; k < N; k++)
+            {
+
+                Furie[k] = new Complex();
+                for (int n = 0; n < N; n++)
+                {
+                    Arg = 2 * Math.PI * k * n / N;
+                    Furie[k].Re += Convert.ToDouble(file[n, 2]) * Math.Cos(Arg);
+                    Furie[k].Im -= Convert.ToDouble(file[n, 2]) * Math.Sin(Arg);
+
+
+                }
+
+                Furie[k].Amplitude = (Math.Sqrt(Math.Pow(Furie[k].Re, 2) + Math.Pow(Furie[k].Im, 2)));
+                Furie[k].Faza = Math.Atan2(Furie[k].Im, Furie[k].Re) / Math.PI * 180;
+                Furie[k].Frecuensy = ((N - 1) * (k));
+
+            }
+
+            Chart4.Series[0].LegendText = "Ось Z";
+            for (uint i = 1; i < kol; i++)
+            {
+                Chart4.Series[0].Points.AddXY(i, Convert.ToDouble(file[i, 2]));
+
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+
+                ChartAFT.Series[0].Points.AddXY(Furie[i].Frecuensy, Furie[i].Amplitude);
+                ChartFFT.Series[0].Points.AddXY(Furie[i].Frecuensy, Furie[i].Faza);
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)//выполняются действия при нажатии на кнопку Сброс.
+        {
+                Chart4.Series[0].Points.Clear();//очищается первый график
+                ChartAFT.Series[0].Points.Clear();//очищается график Амплитудного спектра
+                ChartFFT.Series[0].Points.Clear();//очищается график Фазного спектра
+            
+            
+        }
+    }
+
+
+
+
+    ///все что выделено зеленым цветом, тебе не нужно)
+
+
+    /*
+   //////
+   ///Считывает строки и числа через пробел выносит в отдельные лейблы
+   //////
+   string[,] file = new string[10000, 3];
+   using (StreamReader sr = new StreamReader(@"C:\Users\Asus\Desktop\1.txt", Encoding.Default))
+   {
+       System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+       int i = 0;
+       string input;
+       while ((input = sr.ReadLine()) != null)
+       {
+           string[] m = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+           for (int k = 0, j = 0; k < m.Length; k++)
+           {
+               if (m[k].Trim() != " ")
+               {
+                   file[i, j] = m[k];
+                   j++;
+               }
+           }
+           i++;
+       }
+   }
+   for (int i = 0; i < 2; i++)
+   {
+       LB1.Text = file[0, 0];
+       LBL2.Text = file[0, 1];
+       LBL3.Text = file[0, 2];
+       LBL4.Text = file[1, 0];
+       LBL5.Text = file[1, 1];
+       LBL6.Text = file[1, 2];
+
+
+       Double one = Convert.ToDouble(file[0, 0]);
+       Double two = Convert.ToDouble(file[1, 0]);
+       Double three = one + two;
+       LBL7.Text = Convert.ToString(three) ;
+
+   }
+   */
+
+    /*
+    //////
+    ///Только считывает строку целиком и выводит в лейбл 1
+    //////
+     using (StreamReader sr = new StreamReader(@"C:\Users\Asus\Desktop\1.txt", Encoding.Default))
+    {
+        string s = "";
+        while ((s = sr.ReadLine()) != null)
+        {
+
+            LB1.Text = s;
+        }
+    }
+    */
+}
+
+
+    
+
+
